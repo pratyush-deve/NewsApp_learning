819cd5a11216c37279d9e14003602ca067e81f31
Pratyush Nishank <pratyush.deve@gmail.com>
2025-12-07 08:58:10 +0530

    Making OnBoarding Pages

---
+++ app/src/main/java/com/loc/newsapp/presentation/oboarding/Page.kt
@@ -0,0 +1,30 @@
+package com.loc.newsapp.presentation.oboarding
+
+import androidx.annotation.DrawableRes
+import com.loc.newsapp.R
+
+data class Page (
+    @DrawableRes
+    val image: Int,
+    val title: String,
+    val description: String
+)
+
+val pages = listOf(
+    Page(
+        title = "Lorem Ipsum is simply dummy",
+        description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        image = R.drawable.onboarding1
+    ),
+    Page(
+        title = "Lorem Ipsum is simply dummy",
+        description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        image = R.drawable.onboarding2
+    ),
+    Page(
+        title = "Lorem Ipsum is simply dummy",
+        description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        image = R.drawable.onboarding3
+    )
+)
+
---
+++ app/src/main/java/com/loc/newsapp/presentation/oboarding/dimens.kt
@@ -0,0 +1,10 @@
+package com.loc.newsapp.presentation.oboarding
+
+import androidx.compose.ui.unit.dp
+
+object dimens {
+    val MediumPadding1 = 24.dp
+    val MediumPadding2 = 30.dp
+
+    val IndicatorSize = 14.dp
+}
