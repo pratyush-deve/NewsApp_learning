df943139696803b1b5c845066bcc00c43e324a1e
Pratyush Nishank <pratyush.deve@gmail.com>
2025-12-11 22:12:51 +0530

    Added Navigation and Transparent system bar and navigation bar

---
+++ app/src/main/java/com/loc/newsapp/presentation/navgraph/NavGraph.kt
@@ -0,0 +1,48 @@
+package com.loc.newsapp.presentation.navgraph
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavGraph
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navigation
+import com.loc.newsapp.presentation.oboarding.OnBoardingScreen
+import com.loc.newsapp.presentation.oboarding.OnBoardingViewModel
+
+@Composable
+fun NavGraph(
+    startDestination: String
+){
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = startDestination
+    ) {
+        navigation(
+            route = Route.AppStartNavigation.route,
+            startDestination = Route.OnBoardingScreen.route
+        ){
+            composable(
+                route = Route.OnBoardingScreen.route
+            ){
+                val viewModel: OnBoardingViewModel = hiltViewModel()
+                OnBoardingScreen(
+                    event = viewModel::onEvent
+                )
+            }
+        }
+        navigation(
+            route = Route.NewsNavigation.route,
+            startDestination = Route.NewsNavigatorScreen.route
+        ){
+            composable(
+                route = Route.NewsNavigatorScreen.route
+            ){
+                Text(text = "News Navigator Screen")
+            }
+        }
+    }
+}
